f61163bcb72459a6cb595cd19eed0c513492b050
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2020-11-10 20:38:52 +0500

    Validate combinator

--- mtpylon/utils.py
+++ mtpylon/utils.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-from typing import Any
+from typing import List, Any, NewType, Optional, ForwardRef, Union
 from collections import OrderedDict
 
 from .exceptions import InvalidCombinator
+
+
+long = NewType('long', int)
+int128 = NewType('int128', int)
+int256 = NewType('int256', int)
+double = NewType('double', float)
+
+BASIC_TYPES = [str, bytes, int, long, int128, int256, double]
 
 
 def is_named_tuple(value: type) -> bool:
@@ -23,21 +31,59 @@ def is_named_tuple(value: type) -> bool:
     )
 
 
-def is_valid_combinator(combinator: Any) -> None:
+def is_good_for_combinator(
+        value: Union[type, ForwardRef],
+        constructors: Optional[List[Any]] = None
+) -> bool:
+    """
+    Checks is passed type could be used for combinator.
+    Types that accept for combinators ar basic types or constructors
+
+    Args:
+        value: type to check
+        constructors: availables constructors
+
+    Returns:
+        is passed type good for combinator or not
+    """
+    if constructors is None:
+        constructors = []
+
+    if value in BASIC_TYPES:
+        return True
+
+    if value in constructors:
+        return True
+
+    if isinstance(value, ForwardRef):
+        type_name = value.__forward_arg__
+        constructor_names = [c.__name__ for c in constructors]
+
+        return type_name in constructor_names
+
+    return False
+
+
+def is_valid_combinator(
+        combinator: Any,
+        constructors: Optional[List[Any]] = None
+) -> None:
     """
     Checks is passed value correct combinator or not. Combinator is NamedTuple
     with Meta class. Named tuple attribute types could be one of bare types:
     `str`, 'bytes', `int`, 'long', 'int128', 'int256', 'double' or other
-    combinator/constructor
+    combinator/constructor. All attributes in combinator should be described
+    in order attribute of
 
     Args:
         combinator: type to check is it combinator or not
+        constructors: List of constructors that could be used to build
+                      combinator
 
     Raises:
         InvalidCombinator - when incorrect combinator has been passed
 
     """
-
     c_name = combinator.__name__
 
     if not is_named_tuple(combinator):
@@ -58,3 +104,22 @@ def is_valid_combinator(combinator: Any) -> None:
         raise InvalidCombinator(
             f'Combinator {c_name} should have name attribute in Meta '
         )
+
+    order = getattr(combinator.Meta, 'order', [])
+
+    for attr_name in order:
+        if attr_name not in combinator.__annotations__:
+            raise InvalidCombinator(
+                f"Missed attribute {attr_name} for combinator {c_name}"
+            )
+
+    for (attr_name, attr_type) in combinator.__annotations__.items():
+        if attr_name not in order:
+            raise InvalidCombinator(
+                f"Attribute {attr_name} doesn`t set in {c_name} order"
+            )
+
+        if not is_good_for_combinator(attr_type, constructors):
+            raise InvalidCombinator(
+                f'Attribute type of {attr_name} should be basic or constructor'
+            )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-from typing import NamedTuple
+from typing import NamedTuple, NewType, Union, ForwardRef
 
 import pytest
 
 from mtpylon.exceptions import InvalidCombinator
-from mtpylon.utils import is_named_tuple, is_valid_combinator
+from mtpylon.utils import (
+    is_named_tuple,
+    is_valid_combinator,
+    is_good_for_combinator
+)
 
 
 class BoolTrue(NamedTuple):
@@ -15,6 +19,9 @@ class BoolTrue(NamedTuple):
 class BoolFalse(NamedTuple):
     class Meta:
         name = 'boolFalse'
+
+
+Bool = NewType('Bool', Union[BoolTrue, BoolFalse])
 
 
 class IncorrectNoMetaCombinator(NamedTuple):
@@ -36,6 +43,66 @@ class IncorrectNameCombinator(NamedTuple):
         name = 323
 
 
+class UserCombinator(NamedTuple):
+    id: int
+    name: str
+
+    class Meta:
+        name = 'user'
+        order = ('id', 'name')
+
+
+class NotInOrder(NamedTuple):
+    id: int
+    value: int
+
+    class Meta:
+        name = 'notInOrder'
+        order = ('id',)
+
+
+class MissedAttribute(NamedTuple):
+    id = int
+
+    class Meta:
+        name = 'attributedDidNotDescribed'
+        order = ('id', 'value')
+
+
+class TreeNode(NamedTuple):
+    value: int
+    left_node: 'Tree'
+    right_node: 'Tree'
+
+    class Meta:
+        name = 'treeNode'
+        order = ('value', 'left_node', 'right_node')
+
+
+class LeafNode(NamedTuple):
+    value: int
+
+    class Meta:
+        name = 'leafNode'
+        order = ('value', )
+
+
+Tree = NewType('Tree', Union[TreeNode, LeafNode])
+
+
+class AnotherClass:
+    pass
+
+
+class WrongAttrType(NamedTuple):
+    id: int
+    value: AnotherClass
+
+    class Meta:
+        name = 'wrongAttrType'
+        order = ('id', 'value')
+
+
 class TestIsNamedTuple:
 
     def test_correct(self):
@@ -46,6 +113,28 @@ class TestIsNamedTuple:
 
     def test_wrong_type(self):
         assert not is_named_tuple(int)
+
+
+class TestIsGoodForCombinator:
+
+    def test_basic_type(self):
+        assert is_good_for_combinator(str)
+
+    def test_constructor_type(self):
+        assert is_good_for_combinator(Bool, [Bool, Tree])
+
+    def test_constructor_forward_ref(self):
+        assert is_good_for_combinator(ForwardRef('Tree'), [Bool, Tree])
+
+    def test_constructor_annotation(self):
+        left_node_type = TreeNode.__annotations__['left_node']
+        assert is_good_for_combinator(left_node_type, [Bool, Tree])
+
+    def test_wrong_value(self):
+        assert not is_good_for_combinator(AnotherClass)
+
+    def test_wrong_forward_ref(self):
+        assert not is_good_for_combinator(ForwardRef('Tree'), [Bool])
 
 
 class TestIsValidCombinator:
@@ -68,3 +157,21 @@ class TestIsValidCombinator:
 
         with pytest.raises(InvalidCombinator):
             is_valid_combinator(IncorrectNameCombinator)
+
+    def test_combinator_with_attributes(self):
+        is_valid_combinator(UserCombinator)
+
+    def test_attribute_is_not_in_order(self):
+        with pytest.raises(InvalidCombinator):
+            is_valid_combinator(NotInOrder)
+
+    def test_missed_attribute(self):
+        with pytest.raises(InvalidCombinator):
+            is_valid_combinator(MissedAttribute)
+
+    def test_wrong_attribute(self):
+        with pytest.raises(InvalidCombinator):
+            is_valid_combinator(WrongAttrType)
+
+    def test_recursive_combinator(self):
+        is_valid_combinator(TreeNode, [Bool, Tree])
