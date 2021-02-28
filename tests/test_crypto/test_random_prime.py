c1b3c2b009e47ceb9c51f779751e57a9b5dc69ed
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-02-28 12:47:31 +0300

    Add low leverl primary tests

--- tests/test_crypto/test_random_prime.py
+++ tests/test_crypto/test_random_prime.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
-from mtpylon.crypto.random_prime import random_odd
+import pytest
+
+from mtpylon.crypto.random_prime import (
+    random_odd,
+    is_low_level_test_passed
+)
+
+
+prime_tests = [
+    (4, False),
+    (7, True),
+    (10, False),
+    (53, True),
+    (23438, False),
+    (1201, True),
+    (7853, True),
+    (121231303, True),
+    (121231304, False),
+    (121234591, True),
+]
 
 
 def test_random_odd():
@@ -7,3 +26,12 @@ def test_random_odd():
 
     assert value % 2 == 1
     assert value >> 64 == 1
+
+
+@pytest.mark.parametrize(
+    'value,expected',
+    prime_tests,
+    ids=lambda x: x
+)
+def test_is_low_level_test_passed(value, expected):
+    assert is_low_level_test_passed(value) == expected
