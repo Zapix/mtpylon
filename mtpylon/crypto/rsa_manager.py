57e79558bc43c54d8f44ffa31e1344b8a02e6330
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-03-01 19:12:49 +0500

    Add rsa key manager

--- mtpylon/crypto/rsa_manager.py
+++ mtpylon/crypto/rsa_manager.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from typing import Protocol, List
+from typing import Protocol, List, Dict
 from dataclasses import dataclass
 
 from rsa import PublicKey, PrivateKey  # type: ignore
+
+from .rsa_fingerprint import get_fingerprint
 
 
 @dataclass
@@ -19,8 +21,39 @@ class RsaManagerProtocol(Protocol):
     def __getitem__(self, item: int) -> KeyPair:
         ...
 
-    def get_public_key_list(self) -> List[str]:
+    @property
+    def public_key_list(self) -> List[bytes]:
         ...
 
-    def get_fingerprint_list(self) -> List[int]:
+    @property
+    def fingerprint_list(self) -> List[int]:
         ...
+
+
+class RsaManager(RsaManagerProtocol):
+
+    def __init__(self, rsa_keys: List[KeyPair]):
+        self._key_map: Dict[int, KeyPair] = {
+            get_fingerprint(item.public): item
+            for item in rsa_keys
+        }
+
+    def __contains__(self, item: int) -> bool:
+        return item in self._key_map
+
+    def __getitem__(self, item: int) -> KeyPair:
+        return self._key_map[item]
+
+    @property
+    def public_key_list(self) -> List[bytes]:
+        return [
+            key.public.save_pkcs1()
+            for key in self._key_map.values()
+        ]
+
+    @property
+    def fingerprint_list(self) -> List[int]:
+        return [
+            fingerprint
+            for fingerprint in self._key_map.keys()
+        ]
