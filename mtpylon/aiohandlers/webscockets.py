57cb5a20160b157e72b0dc01e3c63db25a34f16a
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-04-07 00:44:40 +0500

    Move ws_handler out of create_ws_handler factory

--- mtpylon/aiohandlers/webscockets.py
+++ mtpylon/aiohandlers/webscockets.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+from functools import partial
 from typing import cast, Optional, Callable, Awaitable
 
 from aiohttp import WSMsgType
@@ -18,11 +19,9 @@ logger = logging.getLogger(__name__)
 WebSocketHandler = Callable[[Request], Awaitable[WebSocketResponse]]
 
 
-def create_websocket_handler(
-    schema: Schema
-) -> WebSocketHandler:
+async def ws_handler(request: Request, schema: Schema) -> WebSocketResponse:
     """
-    Builds websocket handler that works with MTProto protocol.
+    Websocket handler that works with MTProto protocol.
     MTProto works only with binary data so close connection if not binary
     data has been sent.
     First of all gets mtproto transport obfuscation header to
@@ -31,41 +30,43 @@ def create_websocket_handler(
     If server didn't receive current data - close connection
     """
 
-    async def ws_handler(request: Request) -> WebSocketResponse:
-        ws = WebSocketResponse()
-        await ws.prepare(request)
+    ws = WebSocketResponse()
+    await ws.prepare(request)
 
-        transport_tag: Optional[int] = None
-        obfuscator: Optional[Obfuscator] = None
-        transport_wrapper: Optional[TransportWrapper] = None
+    transport_tag: Optional[int] = None
+    obfuscator: Optional[Obfuscator] = None
+    transport_wrapper: Optional[TransportWrapper] = None
 
-        logger.info("New websocket connection")
+    logger.info("New websocket connection")
 
-        async for msg in ws:
-            if msg.type == WSMsgType.BINARY:
-                logger.debug('Handle income message')
+    async for msg in ws:
+        if msg.type != WSMsgType.BINARY:
+            logger.error('MTProto accepts only binary data')
+            break
+        logger.debug('Handle income message')
 
-                if transport_tag is None:
-                    try:
-                        transport_tag, obfuscator = parse_header(msg.data)
-                        transport_wrapper = get_wrapper(transport_tag)
-                    except ValueError as e:
-                        logger.error(str(e))
-                        break
-                    else:
-                        logger.debug(
-                            f'Obfuscation,wrapper for {hex(transport_tag)} set'
-
-                        )
-                        continue
-            else:
-                logger.error('MTProto accepts only binary data')
+        if transport_tag is None:
+            try:
+                transport_tag, obfuscator = parse_header(msg.data)
+                transport_wrapper = get_wrapper(transport_tag)
+            except ValueError as e:
+                logger.error(str(e))
                 break
+            else:
+                logger.debug(
+                    f'Obfuscation,wrapper for {hex(transport_tag)} set'
+                )
+                continue
 
-            obfuscator = cast(Obfuscator, obfuscator)
-            transport_wrapper = cast(TransportWrapper, transport_wrapper)
+        obfuscator = cast(Obfuscator, obfuscator)
+        transport_wrapper = cast(TransportWrapper, transport_wrapper)
 
-        logger.info("Close websocket connection")
-        return ws
+    logger.info("Close websocket connection")
+    return ws
 
-    return ws_handler
+
+def create_websocket_handler(
+        schema: Schema
+) -> WebSocketHandler:
+
+    return partial(ws_handler, schema=schema)
