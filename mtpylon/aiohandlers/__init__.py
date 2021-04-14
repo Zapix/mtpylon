5ed01bfdc9bd429844afbd7cdf1b55c2b93f2493
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-04-14 13:19:55 +0500

    Smarter imports for aiohandlers

--- mtpylon/aiohandlers/__init__.py
+++ mtpylon/aiohandlers/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from .websockets import create_websocket_handler
+from .schema import schema_view_factory
+from .pub_keys import pub_keys_view
+
+__all__ = [
+    'create_websocket_handler',
+    'schema_view_factory',
+    'pub_keys_view',
+]
