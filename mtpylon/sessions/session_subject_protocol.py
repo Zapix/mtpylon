e55a7375edc9d2c3d5982af706083f1926cd364a
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-04-28 18:19:05 +0500

    Declare protocols to store sesion info

---
+++ mtpylon/sessions/session_subject_protocol.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+from .session_storage_protocol import SessionStorageProtocol
+from .session_oberver_protocol import SessionObserverProtocol
+
+
+class SessionSubjectProtocol(SessionStorageProtocol):
+
+    session_storage: SessionStorageProtocol
+
+    observers: List[SessionObserverProtocol]
+
+    def subscribe(self, observer: SessionObserverProtocol):
+        ...
+
+    def unsubscribe(self, observer: SessionObserverProtocol):
+        ...
