96f87fbacec965fafd0039c9afd92bd2b0ec6cd9
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-05-13 23:17:33 +0500

    Return  handle rpc call strategy

--- mtpylon/message_handler/strategies/selector.py
+++ mtpylon/message_handler/strategies/selector.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
-from mtpylon.messages import MtprotoMessage, UnencryptedMessage
+from mtpylon.messages import MtprotoMessage
 
 from .types import HandleStrategy
 from .handle_unknown_message import handle_unknown_message
 from .handle_unencrypted_message import handle_unencrypted_message
+from .handle_rpc_query_message import handle_rpc_query_message
+from .utils import is_unencrypted_message, is_rpc_call_message
 
 
 def get_handle_strategy(message: MtprotoMessage) -> HandleStrategy:
     """
     Selects how message will be handled by message
     """
-    if isinstance(message, UnencryptedMessage):
+    if is_unencrypted_message(message):
         return handle_unencrypted_message
+
+    if is_rpc_call_message(message):
+        return handle_rpc_query_message
 
     return handle_unknown_message
