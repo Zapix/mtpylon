6afbc8ff309b9718802adf04ac4cb37578d064e2
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-05-21 00:17:56 +0300

    Load/Dump object as part of schema

--- mtpylon/service_schema/serialization/message.py
+++ mtpylon/service_schema/serialization/message.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
+from mtpylon.serialization.schema import LoadFunction, DumpFunction
 from mtpylon.serialization.loaded import LoadedValue
 from mtpylon.serialization.int import dump as dump_int, load as load_int
 from mtpylon.serialization.long import dump as dump_long, load as load_long
-from mtpylon.serialization.object import (
-    dump as dump_object,
-    load as load_object
-)
 
 from ..service_schema import service_schema
 from ..constructors.message import Message
 
 
-def dump(value: Message) -> bytes:
+def dump(value: Message, dump_object: DumpFunction) -> bytes:
     data = service_schema[Message]
+    dumped_body = dump_object(value.body)
 
     return (
         dump_int(data.id) +
         dump_long(value.msg_id) +
         dump_int(value.seqno) +
-        dump_int(len(value.body)) +
-        dump_object(value.body)
+        dump_int(len(dumped_body)) +
+        dumped_body
     )
 
 
-def load(input: bytes) -> LoadedValue[Message]:
+def load(input: bytes, load_object: LoadFunction) -> LoadedValue[Message]:
     msg_id = load_long(input[4:]).value
     seqno = load_int(input[12:]).value
     bytes_count = load_int(input[16:]).value
--- tests/test_service_schema/test_serialization/test_service_schema.py
+++ tests/test_service_schema/test_serialization/test_service_schema.py
@@ -10,8 +10,9 @@ from mtpylon.service_schema.constructors import (
     RpcResult,
     RpcError,
 )
+from mtpylon.serialization import CallableFunc
 
-from tests.echoschema import schema, Reply
+from tests.echoschema import schema, Reply, echo
 
 
 @pytest.fixture
@@ -37,52 +38,47 @@ test_params = [
         id='msgs_ack'
     ),
     pytest.param(
-        Message(
-            msg_id=long(0x5e0b700a00000000),
-            seqno=0,
-            bytes=14,
-            body=b'dumped message'
-        ),
-        (
-            b'\x11\xe5\xb8[' +
-            b'\x00\x00\x00\x00\x0a\x70\x0b\x5e' +
-            b'\x00\x00\x00\x00' +
-            b'\x0e\x00\x00\x00' +
-            b'dumped message'
-        ),
-        id='message'
-    ),
-    pytest.param(
         MessageContainer(
             messages=[
                 Message(
                     msg_id=long(0x5e0b700a00000000),
-                    seqno=0,
-                    bytes=14,
-                    body=b'dumped message'
+                    seqno=7,
+                    bytes=20,
+                    body=MsgsAck(
+                        msg_ids=[
+                            long(1621416313)
+                        ]
+                    ),
                 ),
                 Message(
-                    msg_id=long(0x5e0b800e00000000),
-                    seqno=0,
-                    bytes=22,
-                    body=b'another dumped message'
+                    msg_id=long(0x60a4d9830000001c),
+                    seqno=9,
+                    bytes=16,
+                    body=CallableFunc(
+                        func=echo,
+                        params={'content': 'hello world'}
+                    )
                 ),
             ]
         ),
         (
-            b'HI\xc3\x94' +  # msg_container number
-            b'\x15\xc4\xb5\x1c' +  # vector number
-            b'\x02\x00\x00\x00' +  # vector size
-            b'\x11\xe5\xb8[' +  # message number
-            b'\x00\x00\x00\x00\x0a\x70\x0b\x5e' +  # msg_id
-            b'\x00\x00\x00\x00' +  # seqno
-            b'\x0e\x00\x00\x00' +  # byte size
-            b'dumped message' +  # body
-            b'\x11\xe5\xb8[' +  # message number
-            b'\x00\x00\x00\x00\x0e\x80\x0b\x5e' +  # msg_id
-            b'\x00\x00\x00\x00' +  # seqno
-            b'\x16\x00\x00\x00' +  # byte size
-            b'another dumped message'  # body
+            b'HI\xc3\x94'  # msg_container number
+            b'\x15\xc4\xb5\x1c'  # vector number
+            b'\x02\x00\x00\x00'  # vector size
+            b'\x11\xe5\xb8['  # message number
+            b'\x00\x00\x00\x00\x0a\x70\x0b\x5e'  # msg_id
+            b'\x07\x00\x00\x00'  # seq No
+            b'\x14\x00\x00\x00'  # meessage length
+            b'\x59\xb4\xd6\x62'  # msg_ack combinator
+            b'\x15\xc4\xb5\x1c'  # vector combinator
+            b'\x01\x00\x00\x00'  # vector size
+            b'\x79\xd9\xa4\x60\x00\x00\x00\x00'  # ack msg id
+            b'\x11\xe5\xb8['  # message number
+            b'\x1c\x00\x00\x00\x83\xd9\xa4\x60'  # message id
+            b'\x09\x00\x00\x00'  # msg seq no
+            b'\x10\x00\x00\x00'  # message length
+            b'\x0a\x3b\x35\xfb'  # echo func
+            b'\x0b\x68\x65\x6c\x6c\x6f\x20\x77\x6f\x72\x6c\x64'  # content str
         ),
         id='msg_container',
     ),
@@ -145,6 +141,49 @@ test_params = [
             b'>\x00j\r,\x00\x00\x00\x0bhello world'
         ),
         id='rpc response reply'
+    ),
+    pytest.param(
+        Message(
+            msg_id=long(0x60a4d98300000018),
+            seqno=7,
+            bytes=20,
+            body=MsgsAck(
+                msg_ids=[
+                    long(1621416313)
+                ]
+            )
+        ),
+        (
+            b'\x11\xe5\xb8['  # message combinator
+            b'\x18\x00\x00\x00\x83\xd9\xa4\x60'  # msgId
+            b'\x07\x00\x00\x00'  # seq No
+            b'\x14\x00\x00\x00'  # meessage length
+            b'\x59\xb4\xd6\x62'  # msg_ack combinator
+            b'\x15\xc4\xb5\x1c'  # vector combinator
+            b'\x01\x00\x00\x00'  # vector size
+            b'\x79\xd9\xa4\x60\x00\x00\x00\x00'  # ack msg id
+        ),
+        id='message with msg ack'
+    ),
+    pytest.param(
+        Message(
+            msg_id=long(0x60a4d9830000001c),
+            seqno=9,
+            bytes=16,
+            body=CallableFunc(
+                func=echo,
+                params={'content': 'hello world'}
+            )
+        ),
+        (
+            b'\x11\xe5\xb8['  # message combinator
+            b'\x1c\x00\x00\x00\x83\xd9\xa4\x60'  # message id
+            b'\x09\x00\x00\x00'  # msg seq no
+            b'\x10\x00\x00\x00'  # message length
+            b'\x0a\x3b\x35\xfb'  # echo func
+            b'\x0b\x68\x65\x6c\x6c\x6f\x20\x77\x6f\x72\x6c\x64'  # content str
+        ),
+        id='message with rpc call'
     )
 ]
 
