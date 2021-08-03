cdaa7b62ad04a6964b2085a01fc3451c1a040269
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-03 14:09:21 +0500

    Add auth_key for session classes

--- mtpylon/sessions/session_storage_protocol.py
+++ mtpylon/sessions/session_storage_protocol.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
 from typing import Protocol
 
-from mtpylon import long
+from mtpylon.types import long
+from mtpylon.crypto import AuthKey
 
 
 class SessionStorageProtocol(Protocol):
 
-    async def create_session(self, session_id: long):  # pragma: nocover
+    async def create_session(
+        self,
+        auth_key: AuthKey,
+        session_id: long
+    ):  # pragma: nocover
         """
         Saves info about new session(session_id)
         """
         ...
 
-    async def has_session(self, session_id: long) -> bool:  # pragma: nocover
+    async def has_session(
+        self,
+        auth_key: AuthKey,
+        session_id: long
+    ) -> bool:  # pragma: nocover
         """
         Checks has session_id been stored or not
         """
         ...
 
-    async def destroy_session(self, session_id: long):  # pragma: nocover
+    async def destroy_session(
+        self,
+        auth_key: AuthKey,
+        session_id: long
+    ):  # pragma: nocover
         """
         Removes info about stored
         """
--- tests/test_middlewares/test_session_id.py
+++ tests/test_middlewares/test_session_id.py
@@ -4,10 +4,12 @@ from unittest.mock import AsyncMock, MagicMock
 import pytest
 
 from mtpylon.types import long
+from mtpylon.crypto import AuthKey
 from mtpylon.constants import SESSION_SUBJECT_RESOURCE_NAME
 from mtpylon.contextvars import (
     income_message_var,
     session_id_var,
+    auth_key_var
 )
 from mtpylon.serialization import CallableFunc
 from mtpylon.messages import EncryptedMessage
@@ -21,8 +23,16 @@ from mtpylon.middlewares.set_session_id import set_session_id
 from tests.simpleschema import set_task
 
 
+@pytest.fixture
+def auth_key():
+    auth_key_data = 19173138450442901591212846731489158386637947003706705793697466005840359118325984130331040988945126053253996441948760332982664467588522441280332705742850565768210808401324268532424996018690635427668017180714026266980907736921933287244346526265760362799421836958089132713806536144155052702458589251084836839798895173633060038337086228492970997360899822298102887831495037141025890442863060204588167094562664112338589266018632963871940922732022252873979144345421549843044971414444544589457542139689588733663359139549339618779617218500603713731236381263744006515913607287705083142719869116507454233793160540351518647758028  # noqa
+    return AuthKey(auth_key_data)
+
+
 @pytest.mark.asyncio
-async def test_create_session_id():
+async def test_create_session_id(auth_key):
+    auth_key_var.set(auth_key)
+
     msg_id = long(0x51e57ac42770964a)
     session_id = long(123123)
     salt = long(234234)
@@ -61,7 +71,8 @@ async def test_create_session_id():
     awaited_arg = session_observer.update.await_args[0][0]
     expected_event = SessionEvent(
         type='created',
-        session_id=session_id
+        session_id=session_id,
+        auth_key=auth_key
     )
     assert awaited_arg == expected_event
 
@@ -69,7 +80,9 @@ async def test_create_session_id():
 
 
 @pytest.mark.asyncio
-async def test_session_id_created_before():
+async def test_session_id_created_before(auth_key):
+    auth_key_var.set(auth_key)
+
     msg_id = long(0x51e57ac42770964a)
     session_id = long(123123)
     salt = long(234234)
@@ -91,7 +104,7 @@ async def test_session_id_created_before():
 
     session_subject = SessionSubject(InMemorySessionStorage)
 
-    await session_subject.create_session(session_id=session_id)
+    await session_subject.create_session(auth_key, session_id)
 
     request = MagicMock()
     request.app = {
